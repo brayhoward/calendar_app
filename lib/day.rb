4a7c768f6750ea057e4d6bb19c9eff4c0add0d79
brayhoward <brandonrhoward@gmail.com>
2015-04-26 12:25:32 -0500

    zellers draft one. not returning correct number.

--- lib/day.rb
+++ lib/day.rb
@@ -4,13 +4,33 @@ class Day
 
 	attr_reader :month, :year
 
+	@@month_index_for_zellers = [nil, 13, 14, 3, 4, 5,
+															6, 7, 8, 9, 10, 11, 12]
+
 	def initialize(month, year)
 		@month = month
 		@year = year
 	end
 
-	def start?
-		2
+	def zellers_month_index
+		@@month_index_for_zellers[@month]
+	end
+
+	def month_start
+		# formula based on Zeller's congruence.
+		# http://en.wikipedia.org/wiki/Zeller's_congruence
+
+		month_day = 1 #q
+		month = zellers_month_index #m
+		century_year = (@year % 100) #K
+		zero_base = (@year / 100)
+
+		h = ((month_day + ((13 * (month + 1)) / 5).floor) + century_year +
+				(century_year / 4).floor + (zero_base / 4).floor + (5 * zero_base))
 	end
 
 end
+
+
+
+
--- test/test_day.rb
+++ test/test_day.rb
@@ -9,9 +9,35 @@ class TestDay < Minitest::Test
 		assert_equal 2015, d.year
 	end
 
-	def test_startofmonth_returns
-		d = Day.new(05, 2015)
-		assert_equal 2, d.start_of_month
+	def test_monthStart_returns_correct_day_number
+		d = Day.new(5, 2015)
+		assert_equal 6, d.month_start
+	end
+
+	def test_zellers_month_index_may
+		d = Day.new(5, 2015)
+		assert_equal 5, d.zellers_month_index
+	end
+
+	def test_zellers_month_index_feb
+		d = Day.new(2, 2015)
+		assert_equal 14, d.zellers_month_index
+	end
+
+	def test_zellers_month_index_jan
+		d = Day.new(1, 2015)
+		assert_equal 13, d.zellers_month_index
+	end
+
+	def test_zellers_month_index_sep
+		d = Day.new(9, 2015)
+		assert_equal 9, d.zellers_month_index
 	end
 
 end
+
+
+
+
+
+
