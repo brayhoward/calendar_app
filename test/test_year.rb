25b3d58b34f86e8dcef4c4b47c8e93f0ce23e607
brayhoward <brandonrhoward@gmail.com>
2015-04-29 11:56:10 -0500

    removed some test that were no longer relevant

---
+++ test/test_year.rb
@@ -0,0 +1,130 @@
+require_relative 'helper'
+require_relative '../lib/year'
+
+class TestYear < Minitest::Test
+
+  def test_year_initiates_year_value
+    y = Year.new(2000)
+    assert_equal 2000, y.year
+  end
+
+  def test_year_is_leap_is_true
+    y = Year.new(2004)
+    assert_equal true, y.leap_year?
+  end
+
+  def test_year_is_leap_is_false
+    y = Year.new(2003)
+    assert_equal false, y.leap_year?
+  end
+
+
+  def test_leap_year_method_for_reg_leap_year
+    y = Year.new(2016)
+    assert_equal true, y.leap_year?
+  end
+
+  def test_leap_year_method_for_century_leap_year
+    y = Year.new(2300)
+    assert_equal false, y.leap_year?
+  end
+
+  def test_leap_year_method_for_400year_leap_year
+    y = Year.new(2400)
+    assert_equal true, y.leap_year?
+  end
+
+  def test_end_of_century_leap_year
+    y = Year.new(2000)
+    assert_equal true, y.leap_year?
+  end
+
+  def test_century_leap_year_1900
+    y = Year.new(1900)
+    assert_equal false, y.leap_year?
+  end
+
+  def test_to_s_output_for_year
+    y = Year.new(1996)
+    expected = <<EOS
+                             1996
+
+      January               February               March
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6               1  2  3                  1  2
+ 7  8  9 10 11 12 13   4  5  6  7  8  9 10   3  4  5  6  7  8  9
+14 15 16 17 18 19 20  11 12 13 14 15 16 17  10 11 12 13 14 15 16
+21 22 23 24 25 26 27  18 19 20 21 22 23 24  17 18 19 20 21 22 23
+28 29 30 31           25 26 27 28 29        24 25 26 27 28 29 30
+                                            31
+       April                  May                   June
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6            1  2  3  4                     1
+ 7  8  9 10 11 12 13   5  6  7  8  9 10 11   2  3  4  5  6  7  8
+14 15 16 17 18 19 20  12 13 14 15 16 17 18   9 10 11 12 13 14 15
+21 22 23 24 25 26 27  19 20 21 22 23 24 25  16 17 18 19 20 21 22
+28 29 30              26 27 28 29 30 31     23 24 25 26 27 28 29
+                                            30
+        July                 August              September
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6               1  2  3   1  2  3  4  5  6  7
+ 7  8  9 10 11 12 13   4  5  6  7  8  9 10   8  9 10 11 12 13 14
+14 15 16 17 18 19 20  11 12 13 14 15 16 17  15 16 17 18 19 20 21
+21 22 23 24 25 26 27  18 19 20 21 22 23 24  22 23 24 25 26 27 28
+28 29 30 31           25 26 27 28 29 30 31  29 30
+                                            
+      October               November              December
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+       1  2  3  4  5                  1  2   1  2  3  4  5  6  7
+ 6  7  8  9 10 11 12   3  4  5  6  7  8  9   8  9 10 11 12 13 14
+13 14 15 16 17 18 19  10 11 12 13 14 15 16  15 16 17 18 19 20 21
+20 21 22 23 24 25 26  17 18 19 20 21 22 23  22 23 24 25 26 27 28
+27 28 29 30 31        24 25 26 27 28 29 30  29 30 31
+                                            
+EOS
+    assert_equal expected, y.to_s
+  end
+
+  def test_to_s_output_for_year
+    y = Year.new(1984)
+    expected = <<EOS
+                             1984
+
+      January               February               March
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7            1  2  3  4               1  2  3
+ 8  9 10 11 12 13 14   5  6  7  8  9 10 11   4  5  6  7  8  9 10
+15 16 17 18 19 20 21  12 13 14 15 16 17 18  11 12 13 14 15 16 17
+22 23 24 25 26 27 28  19 20 21 22 23 24 25  18 19 20 21 22 23 24
+29 30 31              26 27 28 29           25 26 27 28 29 30 31
+                                            
+       April                  May                   June
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7         1  2  3  4  5                  1  2
+ 8  9 10 11 12 13 14   6  7  8  9 10 11 12   3  4  5  6  7  8  9
+15 16 17 18 19 20 21  13 14 15 16 17 18 19  10 11 12 13 14 15 16
+22 23 24 25 26 27 28  20 21 22 23 24 25 26  17 18 19 20 21 22 23
+29 30                 27 28 29 30 31        24 25 26 27 28 29 30
+                                            
+        July                 August              September
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7            1  2  3  4                     1
+ 8  9 10 11 12 13 14   5  6  7  8  9 10 11   2  3  4  5  6  7  8
+15 16 17 18 19 20 21  12 13 14 15 16 17 18   9 10 11 12 13 14 15
+22 23 24 25 26 27 28  19 20 21 22 23 24 25  16 17 18 19 20 21 22
+29 30 31              26 27 28 29 30 31     23 24 25 26 27 28 29
+                                            30
+      October               November              December
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6               1  2  3                     1
+ 7  8  9 10 11 12 13   4  5  6  7  8  9 10   2  3  4  5  6  7  8
+14 15 16 17 18 19 20  11 12 13 14 15 16 17   9 10 11 12 13 14 15
+21 22 23 24 25 26 27  18 19 20 21 22 23 24  16 17 18 19 20 21 22
+28 29 30 31           25 26 27 28 29 30     23 24 25 26 27 28 29
+                                            30 31
+EOS
+    assert_equal expected, y.to_s
+
+  end
+
+end
